f5a0f14283630dc09abba938ed75dcdd926d44c8
Светлана Г <svetlgrish28@gmail.com>
2026-03-27 18:08:15 +0300

    Подключит redux к проекту.

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
-import {Setting} from './const';
+import { Setting } from './const';
 import { offersMock } from './mocks/offers';
 import { reviewsMock } from './mocks/reviews';
+import { Provider } from 'react-redux';
+import App from './components/app/app';
+import { store } from './store';
+import { loadOffers } from './store/action';
 import { offerCardsMock } from './mocks/offer-cards';
 
+store.dispatch(loadOffers(offerCardsMock));
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App
-      offers = {offersMock}
-      reviews = {reviewsMock}
-      offerCards = {offerCardsMock}
-      nearbyOffersCount={Setting.NearOffers}
-    />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App
+        offers={offersMock}
+        reviews={reviewsMock}
+        offerCards={offerCardsMock}
+        nearbyOffersCount={Setting.NearOffers}
+      />
+    </Provider>
+  </React.StrictMode>,
 );
